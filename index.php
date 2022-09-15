66937d8f18d6fbee8c9883872a30a8667184dc16
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2022-09-15 15:34:21 +0200

    Alle vragen beantwoord

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opendag</title>
+</head>
+<body>
+    <div class="question-box">
+        <h3>Welke vakken krijg je in het eerste jaar?</h3>
+        <ul>
+            <li><h4>NATIVE</h4><p>Hier leer je windows applicaties te maken, met codetalen zoals Python, C# en XAML.</p></li>
+            <li><h4>Web Development</h4><p>In dit vak leer je de front en backend van websites coderen, met codetalen zoals Html, css en php.</p></li>
+            <li><h4>Praktijkondersteuning</h4><p>In deze les leer je alles wat je op de werkvloer nodig gaat hebben dat niet programeren is. Zoals presenteren, wireframes maken en in groepen te werken.</p></li>
+            <li><h4>Praktijkwerk</h4><p>In dit vak gebruik je alle theorie van de andere lessen in de praktijk. Je werkt hier 6 uur in de week aan projecten.</p></li>
+            <li><h4>Basisvakken</h4><p>Net als alle andere opleidingen krijg je ook hier rekenen, Nederlands en Engels.</p></li>
+        </ul>
+    </div>
+    <div class="question-box">
+        <h3>Hoe ziet het rooster voor eerstejaars eruit?</h3>
+        <p>In het eerste jaar heb je ongeveer 24 uur lesuren in de week. Van de basisvakken heb je per week maar 1 uur, van de theorievakken NATIVE en Web development krijg je 4/5 uur in de week. Verder heb je elke week 6 uur Praktijkwerk en 2 uurtjes Praktijkondersteuning.</p>
+    </div>
+    <div class="question-box">
+        <h3>Hoe is het jaar ingedeeld?</h3>
+        <p>Elk schooljaar is verdeeld in 2 blokken die allebei een half jaar duuren. Wanneer je een blok niet haalt hoef je maar een halfjaar over te doen, in plaats van een heel schooljaar.</p>
+    </div>
+    <div class="question-box">
+        <h3>Is het mogelijk om te versnellen?</h3>
+        <p>De opleiding duurt normaal gesproken 4 jaar, maar het is mogelijk om de opleiding in 3 jaar af te ronden. Wanneer je versnelt, vlieg je sneller door alle stof heen en hoef je minder lang stage te lopen.</p>
+    </div>
+</body>
+</html>
